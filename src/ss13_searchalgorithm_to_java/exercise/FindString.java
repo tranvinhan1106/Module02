36a586303c13e9957151f65568e79eb7056fca18
tranvinhan <tranvinhan1106@gmail.com>
2023-05-08 19:22:45 +0700

    add .

--- src/ss13_searchalgorithm_to_java/exercise/FindString.java
+++ src/ss13_searchalgorithm_to_java/exercise/FindString.java
@@ -1,4 +1,31 @@
 package ss13_searchalgorithm_to_java.exercise;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class FindString {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhập chuỗi: ");
+        String string = input.nextLine();
+        LinkedList<Character> max = new LinkedList<>();
+        for (int i = 0; i < string.length(); i++) {
+            LinkedList<Character> list = new LinkedList<>();
+            list.add(string.charAt(i));
+            for (int j = i + 1; j < string.length(); j++) { //vòng lặp bên trong
+                if (string.charAt(j) > list.getLast()) {
+                    list.add(string.charAt(j));
+                }
+            }
+            if (list.size() > max.size()) {
+                max.clear();
+                max.addAll(list);
+            }
+            list.clear();
+        }
+        for (Character ch : max) {
+            System.out.print(ch);
+        }
+        System.out.println();
+    }
 }
